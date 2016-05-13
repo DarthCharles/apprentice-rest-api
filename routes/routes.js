3d76779cdbee46c502021461dfb5fa19da340ccd
DarthCharles <carlos.darth@gmail.com>
2016-05-13 14:14:44 -0700

    Add: Routes implementation

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
 var fs = require("fs");
 
+var dirname = get_dirname(__dirname)
+
 module.exports = function(app){
 
-  var dirname = get_dirname(__dirname)
 
   //ROOT PATH
   app.get('/', function(req, res) {
@@ -10,28 +11,63 @@ module.exports = function(app){
   });
 
   //GET A LIST OF ALL GAMES
-  app.get('/all', function (req, res) {
-    res.end();
+  app.get('/games', function (req, res) {
+    fs.readFile( dirname + "/" + "games.json", 'utf8', function (err, data) {
+        games = JSON.parse(data);
+        res.json(games);
+    });
   });
 
   //CREATE A NEW GAME
   app.post('/game', function (req, res) {
-    res.end();
+    fs.readFile( dirname + "/" + "games.json", 'utf8', function (err, data) {
+        var games = JSON.parse(data);
+        var count = Object.keys(games).length
+        console.log(count)
+        var id = count + 1
+        var game =  req.body
+        game.id = id
+
+        games["game" + id ] = game
+        save(games)
+        res.json(game);
+    });
   });
 
   //SHOW A GAME WITH GIVEN ID
   app.get('/game/:id', function (req, res) {
-    res.end();
+    fs.readFile( dirname + "/" + "games.json", 'utf8', function (err, data) {
+        games = JSON.parse(data);
+        var game = games["game" + req.params.id]
+        res.json(game);
+    });
   });
 
   //UPDATE A GAME WITH GIVEN ID
   app.put('/game/:id', function (req, res) {
-    res.end();
+    fs.readFile( dirname + "/" + "games.json", 'utf8', function (err, data) {
+      games = JSON.parse(data);
+      var id = req.params.id
+      var name = req.body.name
+      var game = games["game" + req.params.id]
+
+      game.name = name
+
+      save(games);
+      res.json(game);
+    });
   });
 
   //DELETES A GAME WITH GIVEN ID
   app.delete('/game/:id', function (req, res) {
-    res.end();
+    fs.readFile( dirname + "/" + "games.json", 'utf8', function (err, data) {
+        var games = JSON.parse(data);
+        var id = req.params.id;
+        games["game" + req.params.id] = {}
+
+        save(games)
+        res.json(games);
+    });
   });
 
 };
@@ -40,4 +76,8 @@ module.exports = function(app){
 function get_dirname(str) {
   var res = str.replace("routes", "db");
   return res
+}
+
+function save(games) {
+  fs.writeFileSync(dirname + "/" + "games.json", JSON.stringify(games));
 }
