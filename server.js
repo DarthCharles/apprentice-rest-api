9e14d59d2eb8bc0a92962d12bfaa6614b3b0b922
DarthCharles <carlos.darth@gmail.com>
2016-05-14 20:20:12 -0700

    Add: Gulp for server restart on file update

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ var express = require('express'); // call express
 var bodyParser = require('body-parser');
 var app = express();  // define our app using express
 
+// ROUTES FOR OUR API
+var router =  require('./routes/routes.js')(app);
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-// ROUTES FOR OUR API
-var router =  require('./routes/routes.js')(app);
+app.use(function(req, res, next) {
+  res.status(404).send('Say whaaaaaaat???');
+});
 
 // START THE SERVER
 var port = process.env.PORT || 8080;
