34f960a98d09e3b2ccd3835a13198e47b5356f9b
Ariel-isaacm <ariel.isaacm@gmail.com>
2016-05-23 15:30:23 -0700

    Added CORS policies
    Removed white spaces & other http methods added
    
    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ var app = express();  // define our app using express
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // ROUTES FOR OUR API
 var router =  require('./routes/routes.js')(app);
 
